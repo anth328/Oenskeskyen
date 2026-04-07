7c8fdfeb60784e1d2ca8e3d6c8ae160b83b53868
anth328 <anth0008@stud.ek.dk>
2026-04-08 00:07:19 +0200

    Added Web App Support

---
+++ src/main/java/ek/dk/oenskeskyen/OenskeskyenApplication.java
@@ -0,0 +1,15 @@
+package ek.dk.oenskeskyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class OenskeskyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OenskeskyenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ek/dk/oenskeskyen/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ek.dk.oenskeskyen.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome" + "to the countries website!";
+    }
+}
