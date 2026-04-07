bfcbed801f3d4580c51ed3c8e36870d111c0f270
anth328 <anth0008@stud.ek.dk>
2026-04-07 20:17:15 +0200

    Removed Exlcusion from db

--- src/main/java/ek/dk/oenskeskyen/OenskeSkyenApplication.java
+++ src/main/java/ek/dk/oenskeskyen/OenskeSkyenApplication.java
@@ -2,9 +2,8 @@ package ek.dk.oenskeskyen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class OenskeSkyenApplication {
 
     public static void main(String[] args) {
